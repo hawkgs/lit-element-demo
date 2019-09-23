30a6ed958ef4d5f02669525ba9300c8dfa5663af
hawkgs <hawk.dvp@gmail.com>
2019-09-23 11:44:39 +0300

    Test app-element's contents

---
+++ test/app-element.test.js
@@ -0,0 +1,15 @@
+import { fixture, expect } from '@open-wc/testing';
+
+import '../src/app-element';
+
+describe('app-element', () => {
+  it('should contain header and todo element', async () => {
+    const el = await fixture('<app-element></app-element>');
+    expect(el).shadowDom.to.equal(`
+      <div id="app">
+        <header-element></header-element>
+        <todo-element></todo-element>
+      </div>
+    `);
+  });
+});
