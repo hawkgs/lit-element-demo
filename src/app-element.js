11cc0be8fd428350f930edaf3b74c3038eb40363
hawkgs <hawk.dvp@gmail.com>
2019-09-23 11:37:47 +0300

    Refactor element structure

--- src/app-element.js
+++ src/app-element.js
@@ -1,84 +1,16 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 
-import './header-element';
-import './todo-input';
-import './todo-list';
-
-const INIT_TODOS = [{
-  id: 0,
-  text: 'Groceries',
-  starred: false
-}, {
-  id: 1,
-  text: 'Car servicing',
-  starred: true
-}];
+import './header-element/header-element';
+import './todo-element/todo-element';
 
 class AppElement extends LitElement {
-  constructor() {
-    super();
-    this.todos = [...INIT_TODOS];
-  }
-
-  static get styles() {
-    return css`
-      .main-content {
-        width: 600px;
-        margin: 50px auto 0 auto;
-        background: white;
-        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-      }
-    `;
-  }
-
-  static get properties() {
-    return {
-      todos: Array
-    };
-  }
-
   render() {
     return html`
       <div id="app">
         <header-element></header-element>
-        <div class="main-content">
-          <todo-input
-            .onSubmit=${this.onTodoSubmit.bind(this)}
-          >
-          </todo-input>
-          <todo-list
-            .todos=${this.todos}
-            .onToggleStarring=${this.onToggleStarring.bind(this)}
-            .onDelete=${this.onDelete.bind(this)}
-          >
-          </todo-list>
-        </div>
+        <todo-element></todo-element>
       </div>
     `;
-  }
-
-  onTodoSubmit(todoText) {
-    this.todos = [
-      ...this.todos,
-      {
-        id: Date.now(),
-        text: todoText,
-        starred: false
-      }
-    ];
-  }
-
-  onToggleStarring(id) {
-    this.todos = this.todos.map((t) => {
-      if (t.id === id) {
-        t.starred = !t.starred;
-      }
-      return t;
-    });
-  }
-
-  onDelete(id) {
-    this.todos = this.todos.filter((t) => t.id !== id);
   }
 }
 
---
+++ src/todo-element/todo-element.js
@@ -0,0 +1,81 @@
+import { LitElement, html, css } from 'lit-element';
+
+import './todo-input';
+import './todo-list';
+
+const INIT_TODOS = [{
+  id: 0,
+  text: 'Groceries',
+  starred: false
+}, {
+  id: 1,
+  text: 'Car servicing',
+  starred: true
+}];
+
+class TodoElement extends LitElement {
+  static get styles() {
+    return css`
+      .todo-element {
+        width: 600px;
+        margin: 50px auto 0 auto;
+        background: white;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      todos: Array
+    };
+  }
+
+  constructor() {
+    super();
+    this.todos = [...INIT_TODOS];
+  }
+
+  render() {
+    return html`
+      <div class="todo-element">
+        <todo-input
+          .onSubmit=${this.onTodoSubmit.bind(this)}
+        >
+        </todo-input>
+        <todo-list
+          .todos=${this.todos}
+          .onToggleStarring=${this.onToggleStarring.bind(this)}
+          .onDelete=${this.onDelete.bind(this)}
+        >
+        </todo-list>
+      </div>
+    `;
+  }
+
+  onTodoSubmit(todoText) {
+    this.todos = [
+      ...this.todos,
+      {
+        id: Date.now(),
+        text: todoText,
+        starred: false
+      }
+    ];
+  }
+
+  onToggleStarring(id) {
+    this.todos = this.todos.map((t) => {
+      if (t.id === id) {
+        t.starred = !t.starred;
+      }
+      return t;
+    });
+  }
+
+  onDelete(id) {
+    this.todos = this.todos.filter((t) => t.id !== id);
+  }
+}
+
+customElements.define('todo-element', TodoElement);
